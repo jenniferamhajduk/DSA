542f0dff18d1b00e8831ad51556196c4875aec73
Jennifer Hajduk <jhizzle@Jennifers-Air.localdomain>
2021-07-19 23:26:39 -0700

    added find_a_number

--- Interview_problems/pairs.py
+++ Interview_problems/pairs.py
@@ -1,6 +1,6 @@
 target = 9
 pairs = []
-list_of_nums = [2,4,5,7,8,6]
+list_of_nums = [2,6,3,9,11]
 lon  = [num for num in list_of_nums if num <= target]
 for i in range(len(lon)):
     for j in range(len(lon)):
@@ -8,6 +8,9 @@ for i in range(len(lon)):
             pairs_list = []
             pairs_list.append(lon.index(lon[i]))
             pairs_list.append(lon.index(lon[j]))
-            pairs.append(pairs_list)
+            if pairs_list not in pairs:
+                pairs.append(pairs_list)
+            else:
+                pass
             pairs_list = []
 print(pairs)
