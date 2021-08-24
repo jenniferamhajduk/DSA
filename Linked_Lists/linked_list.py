3e2613b12217b4f18faf0926babed53ef817ab96
Jennifer Hajduk <jhizzle@Jennifers-Air.localdomain>
2021-08-23 20:50:10 -0700

    singly linked list

--- Linked_Lists/linked_list.py
+++ Linked_Lists/linked_list.py
@@ -1,22 +1,54 @@
 #Create head and tail and initialize with null
 #Create blank node and assign value to it and reference to null
 #Link head and tail with this node
-class SLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
 
 class Node:
     def __init__(self, value = None):
         self.value = value
         self.next = None
+
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
     def __iter__(self):
         node = self.head
+        while node:
+            yield node
+            node = node.next
+    def insertSLL(self, value, location):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+                if tempNode == self.tail:
+                    self.tail=newNode
 
-sll = SLinkedList()
-node_1 = Node(1)
-node_2 = Node(2)
 
-SLinkedList().head = node_1
-SLinkedList().head.next = node_2
-SLinkedList().tail = node_2                                                                   
+singlyLinkedList = SLinkedList()
+singlyLinkedList.insertSLL(1,1)
+singlyLinkedList.insertSLL(2,1)
+singlyLinkedList.insertSLL(3,1)
+singlyLinkedList.insertSLL(4,1)
+singlyLinkedList.insertSLL(0,0)
+singlyLinkedList.insertSLL(0,3)
+
+print([node.value for node in singlyLinkedList])
