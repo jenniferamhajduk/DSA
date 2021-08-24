82cb1f2593b1326989fe7ef76e90d695c5f0e233
Jennifer Hajduk <jhizzle@Jennifers-Air.localdomain>
2021-08-23 23:32:59 -0700

    completed singly linked list

--- Linked_Lists/linked_list.py
+++ Linked_Lists/linked_list.py
@@ -41,6 +41,60 @@ class SLinkedList:
                 newNode.next = nextNode
                 if tempNode == self.tail:
                     self.tail=newNode
+    def traverseSSL(self):
+        if self.head is None:
+            print("The Singly Licked List does not exist")
+        else:
+            node = self.head
+            while node is not None:
+                print(node.value)
+                node = node.next
+    def searchSLL(self, nodeValue):
+        if self.head is None:
+            return "The list does not exist"
+        else:
+            node = self.head
+            while node is not None:
+                if node.value == nodeValue:
+                    return node.value
+                node = node.next
+            return "the value does not exist in the list"
+    def deleteNode(self, location):
+        if self.head == None:
+            return "The list does not exist"
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail =  None
+                else:
+                    self.head = self.head.next
+            elif location == 1:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail =  None
+                else:
+                    node = self.head
+                    while node is not None:
+                        if node.next == self.tail:
+                            break
+                        node = node.next
+                    node.next = None
+                    self.tail = node
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = nextNode.next
+    def deleteSLL(self):
+        if self.head == None:
+            return "The single linked lsit does not exist"
+        else:
+            self.head = None
+            self.tail = None
 
 
 singlyLinkedList = SLinkedList()
@@ -51,4 +105,10 @@ singlyLinkedList.insertSLL(4,1)
 singlyLinkedList.insertSLL(0,0)
 singlyLinkedList.insertSLL(0,3)
 
+print([node.value for node in singlyLinkedList])
+singlyLinkedList.traverseSSL()
+singlyLinkedList.deleteNode(1)
+print([node.value for node in singlyLinkedList])
+print(singlyLinkedList.searchSLL(3))
+singlyLinkedList.deleteSLL()
 print([node.value for node in singlyLinkedList])
